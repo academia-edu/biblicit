0be087dcd12f32e2994cab27affaf1ed35a192f9
David Judd <david@academia.edu>
2013-03-18 16:31:19 -0700

    Extract citation information from papers using ParsCit

--- lib/biblicit/parscit.rb
+++ lib/biblicit/parscit.rb
@@ -26,11 +26,29 @@ module ParsCit
 
     def parse(xml)
       parsed = xml.css("algorithm[name=ParsHed]")
-      {
+
+      result = {
         title: parsed.css('title').text.gsub(/\s+/,' ').strip,
         authors: parsed.css('author').map { |a| a.text.gsub(/\s+/,' ').strip },
         abstract: parsed.css('abstract').text
       }
+
+      citations = xml.css('algorithm[name=ParsCit] > citationList > citation').map do |node|
+        cite = {
+          authors: node.css('author').map(&:text).map(&:strip).reject(&:blank?).uniq
+        }
+
+        node.children.each do |child|
+          unless ['contexts','authors','marker','rawString'].include?(child.name) || (text = child.text.strip).blank?
+            cite[child.name.to_sym] = text
+          end
+        end
+
+        cite
+      end
+
+      result[:citations] = citations
+      result
     end
 
   end
