6d568754485540b4cb684d74d5c1fd5deec95a2a
David Judd <david@academia.edu>
2013-06-13 16:05:00 -0700

    Export version

--- lib/biblicit.rb
+++ lib/biblicit.rb
@@ -4,4 +4,5 @@ require 'active_support/core_ext/object'
 require 'active_support/core_ext/string'
 require 'active_support/core_ext/hash'
 
+require 'biblicit/version'
 require 'biblicit/extractor'
--- lib/biblicit/version.rb
+++ lib/biblicit/version.rb
@@ -2,6 +2,6 @@
 
 module Biblicit
 
-  VERSION = '2.3.0'
+  VERSION = '2.3.1'
 
 end
